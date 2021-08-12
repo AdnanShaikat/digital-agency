93ba2660a714d60a453d8af7c8c79f9d4006167f
Shaikat Hossain <shaikathossainskth@gmail.com>
2021-08-12 23:59:48 +0600

    Responsive Sidebar

--- components/TheHeader.vue
+++ components/TheHeader.vue
@@ -7,7 +7,7 @@
           <nuxt-link to="/"><p>BRAND</p></nuxt-link>
         </div>
         <!-- nav -->
-        <!-- <div class="hidden lg:block">
+        <div class="hidden lg:block">
           <ul class="m-0 p-0 h-100 flex items-center z-0">
             <li class="font-bold h-100 hover:text-primary">
               <nuxt-link class="px-3" to="/home">Home</nuxt-link>
@@ -28,59 +28,11 @@
               <nuxt-link class="px-3" to="/contact">Contact</nuxt-link>
             </li>
           </ul>
-        </div> -->
-
-        <!-- hidden nav -->
-        <div class="lg:block" :class="{ hidden: hideNav }">
-          <ul
-            class="m-0 p-0 h-100 lg:flex absolute right-0 top-full h-auto w-full items-center bg-white lg:bg-transparent lg:static z-0"
-          >
-            <li class="font-bold h-100">
-              <nuxt-link
-                class="p-0 lg:px-3 lg:pb-0 inline-block pb-5"
-                to="/home"
-                >Home</nuxt-link
-              >
-            </li>
-            <li class="font-bold h-100">
-              <nuxt-link
-                class="p-0 lg:px-3 lg:pb-0 inline-block pb-5"
-                to="/about"
-                >About Us</nuxt-link
-              >
-            </li>
-            <li class="font-bold h-100">
-              <nuxt-link
-                class="p-0 lg:px-3 lg:pb-0 inline-block pb-5"
-                to="/service"
-                >Service</nuxt-link
-              >
-            </li>
-            <li class="font-bold h-100">
-              <nuxt-link
-                class="p-0 lg:px-3 lg:pb-0 inline-block pb-5"
-                to="/pages"
-                >Pages</nuxt-link
-              >
-            </li>
-            <li class="font-bold h-100">
-              <nuxt-link
-                class="p-0 lg:px-3 lg:pb-0 inline-block pb-5"
-                to="/blog"
-                >Blog</nuxt-link
-              >
-            </li>
-            <li class="font-bold h-100">
-              <nuxt-link
-                class="p-0 lg:px-3 lg:pb-0 inline-block pb-5"
-                to="/contact"
-                >Contact</nuxt-link
-              >
-            </li>
-          </ul>
         </div>
 
-        <!-- <div class="hidden lg:block">
+        <!-- hidden nav -->
+
+        <div class="hidden lg:block">
           <div class="flex items-center justify-items-center">
             <i class="fas fa-search z-20"></i>
             <div
@@ -93,66 +45,71 @@
               <button class="text-white z-10">Call Now: 01627125430</button>
             </div>
           </div>
-        </div> -->
+        </div>
 
-        <!-- <div class="menu-bar lg:hidden z-20">
+        <div class="menu-bar lg:hidden z-20">
           <i @click="toggleBar" class="fas fa-bars text-primary text-2xl"></i>
-        </div> -->
+        </div>
       </div>
     </nav>
 
     <!-- Sidebar Nav -->
 
-    <!-- <div class="sidebar lg:hidden" :class="{ hidden: hideNav }">
-      
-      <div class="sidebar__container">
-        <ul class="sidebar__links mt-5">
-          <li class="sidebar__item font-bold h-100 ">
-            <nuxt-link class="" to="/">Home</nuxt-link>
-          </li>
+    <div class="sidebar lg:hidden">
+      <transition name="fade">
+        <div v-if="hideNav" class="sidebar__container">
+          <ul class="sidebar__links mt-5">
+            <li class="sidebar__item font-bold h-100">
+              <nuxt-link class="" to="/">Home</nuxt-link>
+            </li>
 
-          <li class="sidebar__item font-bold h-100 ">
-            <nuxt-link class="" to="/about">About Us</nuxt-link>
-          </li>
+            <li class="sidebar__item font-bold h-100">
+              <nuxt-link class="" to="/about">About Us</nuxt-link>
+            </li>
 
-          <li class="sidebar__item font-bold h-100 ">
-            <nuxt-link class="" to="/service">Service</nuxt-link>
-          </li>
+            <li class="sidebar__item font-bold h-100">
+              <nuxt-link class="" to="/service">Service</nuxt-link>
+            </li>
 
-          <li class="sidebar__item font-bold h-100 ">
-            <nuxt-link class="" to="/pages">Pages</nuxt-link>
-          </li>
+            <li class="sidebar__item font-bold h-100">
+              <nuxt-link class="" to="/pages">Pages</nuxt-link>
+            </li>
 
-          <li class="sidebar__item font-bold h-100 ">
-            <nuxt-link class="" to="/blog">Blog</nuxt-link>
-          </li>
+            <li class="sidebar__item font-bold h-100">
+              <nuxt-link class="" to="/blog">Blog</nuxt-link>
+            </li>
 
-          <li class="sidebar__item font-bold h-100 ">
-            <nuxt-link class="" to="/Contact">Contact</nuxt-link>
-          </li>
-        </ul>
+            <li class="sidebar__item font-bold h-100">
+              <nuxt-link class="" to="/Contact">Contact</nuxt-link>
+            </li>
+          </ul>
 
-        <div class="mt-5" style="padding: 10px 20px;">
-          <div class="flex flex-col items-center justify-center">
-            <div
-              class="text-center text-white bg-primary w-full py-4 mb-2 rounded"
-            >
-              <i class="fas fa-search"></i>
-            </div>
-            <div
-              style="
-                background-image: linear-gradient(#ff5464, #fc9547);
-              "
-              class="rounded w-full text-center py-4"
-            >
-              <button class="text-white">
-                Call Now: 01627125430
-              </button>
+          <div class="mt-5" style="padding: 10px 20px">
+            <div class="flex flex-col items-center justify-center">
+              <div
+                class="
+                  text-center text-white
+                  bg-primary
+                  w-full
+                  py-4
+                  mb-2
+                  rounded
+                "
+              >
+                <i class="fas fa-search"></i>
+              </div>
+              <div
+                style="background-image: linear-gradient(#ff5464, #fc9547)"
+                class="rounded w-full text-center py-4"
+              >
+                <button class="text-white">Call Now: 01627125430</button>
+              </div>
             </div>
           </div>
         </div>
-      </div>
-    </div> -->
+      </transition>
+    </div>
+
     <!-- End Sidebar Nav -->
 
     <TheHero />
@@ -165,14 +122,16 @@ export default {
   components: { TheHero },
   data() {
     return {
-      hideNav: false
+      hideNav: false,
+      sideNav: false,
     };
   },
   methods: {
     toggleBar() {
       this.hideNav = !this.hideNav;
-    }
-  }
+      this.sideNav = !this.sideNave;
+    },
+  },
 };
 </script>
 
@@ -182,5 +141,23 @@ export default {
 }
 nav button {
   padding: 5px 15px;
+}
+.fade-enter-from {
+  transform: translateX(100%);
+}
+.fade-enter-to {
+  transform: translateX(0%);
+}
+.fade-enter-active {
+  transition: all 300ms ease-out;
+}
+.fade-leave-from {
+  transform: translateX(0%);
+}
+.fade-leave-to {
+  transform: translateX(100%);
+}
+.fade-leave-active {
+  transition: all 300ms ease-out;
 }
 </style>
--- components/TheHero.vue
+++ components/TheHero.vue
@@ -1,19 +1,24 @@
 <template>
   <div class="container mx-auto">
     <div
-      class="hero flex flex-col items-center lg:flex-row lg:justify-between mt-28"
+      class="
+        hero
+        flex flex-col
+        items-center
+        lg:flex-row
+        lg:justify-between
+        mt-28
+      "
     >
       <div class="hero__text">
         <p class="text-lg text-primary font-bold">Welcome</p>
-        <p class="text-5xl font-bold">
-          Smart Ideas For Your Brand Here
-        </p>
+        <p class="text-5xl font-bold">Smart Ideas For Your Brand Here</p>
         <p class="mt-4">
           Lorem ipsum dolor sit amet, consectetur adipiscing elit.Cras eu tortor
           pulvinar eros vulputate consequa
         </p>
         <div
-          style="background-image: linear-gradient(#FF5464, #FC9547);"
+          style="background-image: linear-gradient(#ff5464, #fc9547)"
           class="rounded inline-block mt-4"
         >
           <button class="text-white py-4 px-8">CONTACT US NOW</button>
